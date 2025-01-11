e532c12717bf9236f9829fa73ed46eca51951e76
carlo <kirollosedhom@gmail.com>
2025-01-11 12:49:48 +0200

    session 11

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todo/new_note.dart';
+import 'package:todo/notes_page.dart';
+
+import 'notes_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  //test test
+  //test test
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Notes(),
+    );
+  }
+}
